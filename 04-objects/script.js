172ee5e5e446e5f542de93e7ee88ad3cc9cc24c7
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2024-07-04 22:07:27 +0300

    added other tasks

---
+++ 04-objects/script.js
@@ -0,0 +1,70 @@
+/* console.log('Hello')
+const cat = {
+  'legs': 4,
+  'name': 'Harmony',
+  'color': 'Tortoiseshell'
+}
+console.log(cat)
+console.log(cat['legs'])
+console.log(cat.name) */
+
+/* const dog = {
+  name: 'Pancake',
+  age: 6,
+  color: 'white',
+  bark: 'Yip yap yip!'
+}
+console.log(dog)
+console.log(Object.keys(dog).length) // 4
+dog.surName = 'Gappy'
+console.log(dog)
+dog['ears'] = 2
+console.log(dog)
+dog['can help'] = true
+console.log(dog)
+console.log(typeof(dog['can help'])) */
+
+//Array of objects
+/* const anna = { name: 'Anna', age: 11, luckyNumbers: [2, 4, 8, 16]}
+const dave = { name: 'Dave', age: 5, luckyNumbers: [3, 9, 40]}
+const kate = { name: 'Kate', age: 9, luckyNumbers: [1, 2, 3]}
+const friends = [anna, dave, kate]
+console.log(friends) */
+
+//Info about movies
+/* const movies = {
+  'Finding Nemo': {
+    releaseDate: 2003,
+    duration: 100,
+    actors: ['Albert Brooks', 'Ellen DeGeneres', 'Alexander Gould'],
+    format: 'DVD'
+  },
+  'Star Wars: Episode VI - Return of the Jedi': {
+    releaseDate: 1983,
+    duration: 134,
+    actors: ['Mark Hamill', 'Harrison Ford', 'Carrie Fisher'],
+    format: 'DVD'
+  },
+  'Harry Potter and the Goblet of Fire': {
+    releaseDate: 2005,
+    duration: 157,
+    actors: ['Daniel Radcliffe', 'Emma Watson', 'Rupert Grint'],
+    format: 'Blu-ray'
+  },
+}
+const cars = {
+  releaseDate: 2006,
+    duration: 117,
+    actors: ['Owen Wilson', 'Bonnie Hunt', 'Paul Newman'],
+    format: 'Blu-ray'
+}
+movies['Cars'] = cars
+console.log(Object.keys(movies)) */
+
+//Extract the number from the object
+/* const myCrazyObject = {
+  'name': 'A ridiculous object',
+  'some array': [7, 9, { purpose: 'confusion', number: 123 }, 3.3],
+  'random animal': 'Banana Shark'
+}
+console.log(myCrazyObject['some array'][2].number) */
